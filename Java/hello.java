8b479a2cc564945ab85e50c8d1dba67ec72853a9
Johan Sebastian <65955878+JohanSanSebastian@users.noreply.github.com>
2021-03-29 11:46:45 +0400

    Add files via upload

---
+++ Java/hello.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main 
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        String Name;
+        System.out.print("What is your name? ");
+        name = sc.nextLine();
+        System.out.println("Hello" + name);
+
+    }
+}
---
+++ TS/hello.ts
@@ -0,0 +1,5 @@
+let msg: string = "Hello MLH";
+let num = 4;
+
+console.log(msg);
+console.log(num);
